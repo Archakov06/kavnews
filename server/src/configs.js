51c5a3f0b13538c28f4e19b5a0c895516a7b371a
Archakov Dennis <hello@archakov.im>
2017-10-12 12:21:22 +0300

    init tutorial-2

---
+++ server/src/configs.js
@@ -0,0 +1,22 @@
+const elems = {
+  riadagestan: {
+    title: '.itemTitle',
+    image: '.preview_picture',
+    text: '#qaz',
+    views: '.itemHits b'
+  },
+  groznyinform: {
+    title: '.news h1',
+    image: '.imgA img',
+    text: '.news p',
+    views: '.news p.views'
+  },
+  magastimes: {
+    title: '.td-post-title .entry-title',
+    image: '.size-full',
+    text: '.td-post-content p',
+    views: '.td-post-views span'
+  },
+};
+
+export { elems };
---
+++ server/src/index.js
@@ -0,0 +1,7 @@
+import parsePost from './parsePost';
+import { elems } from './configs';
+
+parsePost(
+  'http://grozny-inform.ru/news/politic/89355/',
+  elems.groznyinform
+);
